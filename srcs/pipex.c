df2566ee46002ac0da94cd63b1ea917837044404
Avery Gan <agan@c2r5s14.42singapore.sg>
2023-10-11 19:59:53 +0800

    updated pipex to exec two commands, redirect output to file2

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -12,41 +12,41 @@
 
 #include "pipex.h"
 
-// Fork process
-// Child process cats file1
-// dup2 to redirect stdout to cmd1
-// Use split to obtain char **
-
 int	main(int argc, char *argv[])
 {
 	int pipes[2];
 	int	pid;
-	int pid2;
 	char **args;
 
 	if (pipe(pipes) < 0)
 		return 2;
 	pid = fork();
-	pid2 = fork();
 	if (pid < 0)
 		return 3;
-	// In child process, cat file
+	int fd = open(argv[1], O_RDONLY);
+	// In child process, run grep (cmd1)
 	if (pid == 0)
 	{
 		(void)argc;
 		close(pipes[0]);
-		// Redirect stdout to write pipe
+		// Redirect file1 to stdin
+		dup2(fd, STDIN_FILENO);
 		dup2(pipes[1], STDOUT_FILENO);
-		execve("/bin/cat", (char *[]){"cat", argv[1], NULL}, NULL);
-		close(pipes[1]);
-	}
-	// In parent process, read from pipe and exec
-	else
-	{
-		close(pipes[1]);
-		dup2(pipes[0], STDIN_FILENO);
 		args = ft_split(argv[2], ' ');
 		execve("/bin/grep", args, NULL);
+		close(pipes[1]);
+	}
+	else
+	{
+	// In parent process, exec wc (cmd2)
+		close(pipes[1]);
+		// Read from output of cmd1 to stdin 
+		dup2(pipes[0], STDIN_FILENO);
+		int outfd = open(argv[4], O_WRONLY | O_CREAT, 0644);
+		// Redirect output of cmd2 to stdout
+		dup2(outfd, STDOUT_FILENO);
+		args = ft_split(argv[3], ' ');
+		execve("/bin/wc", args, NULL);
 		close(pipes[0]);
 		wait(NULL);
 	}
